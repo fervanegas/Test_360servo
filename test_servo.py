d2ff3f730e803661e8755cc1813e0b9fa4c23b7e
Fernando Vanegas <fervanegas@gmail.com>
2022-09-12 21:52:20 +1000

    Added python file with example

---
+++ test_servo.py
@@ -0,0 +1,14 @@
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(4)
+
+for x in range(6):
+    servo.min()
+    sleep(1.0)
+    servo.mid()
+    sleep(2.0)
+    servo.max()
+    sleep(1.0)
+    print("Testing servo loop" + str(x))
+print("Finished testing servo")
